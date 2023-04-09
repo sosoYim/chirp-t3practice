300b5d4e248c7c4d14715b7c205e9db4a92a8641
sosoYim <sohyung.yim@gmail.com>
2023-04-09 17:31:16 +0900

    feat: Router
    - prisma에 디비 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "@/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -15,7 +18,12 @@ const Home: NextPage = () => {
         <div>
           {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-        </div>{" "}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
