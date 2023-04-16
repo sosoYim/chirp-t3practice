36c302fab1cd4cb09bd7635f934831cb2dc10eb5
sosoYim <sohyung.yim@gmail.com>
2023-04-16 21:18:41 +0900

    feat: create posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { type User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
@@ -45,4 +49,21 @@ export const postRouter = createTRPCRouter({
       };
     });
   }),
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(250),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
